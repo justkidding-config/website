5efae4c0b6cc2580d9aa9d819d83da7f2c4b09aa
Damien Lespiau <damien@weave.works>
2019-01-08 17:15:46 +0000

    ci: Tune the release script to work in CI

--- run-release.sh
+++ run-release.sh
@@ -1,10 +1,25 @@
-#!/bin/bash
+#!/bin/bash -e
 
-echo "==> building site"
-npm run build
+if [ -z "$CI" ]; then
+
+# Not on CI
+git_url="git@github.com:justkidding-config/justkidding-config.github.io.git"
+
+else
+
+# CI
+git_url="https://${GITHUB_TOKEN}@github.com/jkcfg/jkcfg.github.io.git"
+
+echo "==> setting up git"
+git config --global user.email "damien.lespiau+jkbot@gmail.com"
+git config --global user.name "jkbot"
+
+fi
+
+echo "==> cloning site"
+git clone $git_url deploy
 
 echo "==> deploying site"
-git clone git@github.com:justkidding-config/justkidding-config.github.io.git deploy
 cp -r dist/* deploy/
-(cd deploy && git add . && git commit -m "publish site" && git push)
+(cd deploy && git add . && git commit -m "automatic site publication" && git push)
 rm -rf deploy
